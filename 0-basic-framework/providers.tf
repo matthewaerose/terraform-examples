a0b1266149175aecdd7ef42066d2b75b147e6a58
Matthew <matthewaerose@gmail.com>
2023-09-01 09:18:03 -0500

    udpate terraform and remove provideres file

--- 0-basic-framework/providers.tf
+++
@@ -1,8 +0,0 @@
-providers {
-    required_providers = {
-        random {
-            source = "hashicorp/random"
-            version = ">= 1.0.0"
-        }
-    }
-}
--- 0-basic-framework/terraform.tf
+++ 0-basic-framework/terraform.tf
@@ -1,3 +1,10 @@
 terraform {
-    required_version ">= 1.0.0"
+    required_version = ">= 1.0.0"
+
+    required_providers {
+      random = {
+        source  = "hashicorp/random"
+        version = "3.1.0"
+      }
+    }
 }
